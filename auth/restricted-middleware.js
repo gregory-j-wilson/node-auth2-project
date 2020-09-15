7439cd8ce2dcf7c0fcc798cb6b8e8b598338ae50
Gregory Wilson <gjwilson7390@gmail.com>
2020-09-15 16:19:40 -0500

    put the instructed block message for incorrect token

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken");
 
 
 module.exports = (req, res, next) => {
-    
+
     const token = req.headers.authorization;
     const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
     if (token) {
         jwt.verify(token, secret, (err, decodedToken) => {
             if (err) {
-                res.status(401).json({ message: "Not Allowed" });
+                res.status(401).json({ message: "You shall not pass!" });
             } else {
                 req.jwt = decodedToken;
                 next();
